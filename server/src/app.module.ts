d7ce6497cf4edda9e94e58dc8ddd1d89bd254dcd
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-01-13 17:02:58 +0100

    feat: nest server successfully connected to pg database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'cesco',
+      password: 'database',
+      database: 'BorrowLend',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
