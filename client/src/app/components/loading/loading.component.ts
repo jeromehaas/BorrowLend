d1fd72b1ad345501ffc5c286f01a54e73de9c9a3
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-01-15 12:07:13 +0100

    feat: user set in the login component and retrieved in the loading component

--- client/src/app/components/loading/loading.component.ts
+++ client/src/app/components/loading/loading.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store, select } from '@ngrx/store';
+import { AppState } from 'src/app/app.state';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
-  styleUrls: ['./loading.component.scss']
+  styleUrls: ['./loading.component.scss'],
 })
 export class LoadingComponent implements OnInit {
-
-  constructor() { }
+  user: User;
+  user$ = this.store.pipe(select('user'));
+  constructor(private router: Router, private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.user$.subscribe((user) => {
+      this.user = user;
+      if (user) {
+        setTimeout(() => {
+          console.log('this.user :>> ', this.user);
+          this.router.navigate(['']);
+        }, 5000);
+      }
+    });
   }
-
 }
--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Store, select } from '@ngrx/store';
+import { AppState } from '../../app.state';
+import { setUser } from '../../actions/users.actions';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private router: Router) {}
+  user$ = this.store.pipe(select((state) => state.user));
+
+  constructor(
+    private router: Router,
+    private store: Store<AppState>,
+    private userService: UserService
+  ) {}
 
   onClickSubmit(value): void {
+    this.userService.getUser(value.userName).subscribe((user) => {
+      this.store.dispatch(setUser({ user }));
+    });
     this.router.navigate(['/loading']);
   }
 
