e8e1a71b9a4a20452ee0b867e05b023e9e3aa4f8
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-01-15 12:52:08 +0100

    feat: in the user services added functions for adding/removing an item from the to borrow/lend list

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -15,4 +15,28 @@ export class UserService {
   getUser(username: string): Observable<User> {
     return this.http.get<User>(`${this.userUrl}/username/${username}`);
   }
+
+  addToToBorrowList(userId: number, itemId: number): Observable<User> {
+    return this.http.get<User>(
+      `${this.userUrl}/toBorrowListAdd/${userId}/${itemId}`
+    );
+  }
+
+  removeFromToBorrowList(userId: number, itemId: number): Observable<User> {
+    return this.http.get<User>(
+      `${this.userUrl}/toBorrowListRem/${userId}/${itemId}`
+    );
+  }
+
+  addToToLendList(userId: number, itemId: number): Observable<User> {
+    return this.http.get<User>(
+      `${this.userUrl}/toLendListAdd/${userId}/${itemId}`
+    );
+  }
+
+  removeFromToLendList(userId: number, itemId: number): Observable<User> {
+    return this.http.get<User>(
+      `${this.userUrl}/toLendListRem/${userId}/${itemId}`
+    );
+  }
 }
