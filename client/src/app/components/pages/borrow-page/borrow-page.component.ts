7f3796091340993fe0c5d1574b701a6aab8f6a83
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-01-17 18:37:07 +0100

    feat: an item is shown in the Borrow section only if it is not already in a request

--- client/src/app/components/pages/borrow-page/borrow-page.component.ts
+++ client/src/app/components/pages/borrow-page/borrow-page.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.state';
+import { Item } from 'src/app/models/item';
 import { User } from 'src/app/models/user';
+import { ExchangeService } from 'src/app/services/exchange.service';
 
 @Component({
   selector: 'app-borrow-page',
@@ -11,12 +13,46 @@ import { User } from 'src/app/models/user';
 export class BorrowPageComponent implements OnInit {
   user: User;
   user$ = this.store.pipe(select('user'));
+  itemsToShow: Item[] = [];
 
-  constructor(private store: Store<AppState>) {}
+  constructor(
+    private store: Store<AppState>,
+    private exchangeService: ExchangeService
+  ) {}
 
   ngOnInit(): void {
     this.user$.subscribe((user) => {
-      this.user = user;
+      if (user) {
+        this.itemsToShow = [];
+        this.user = user;
+        if (this.user.exchangesBorr.length) {
+          let isInExchange = false;
+          this.user.toBorrowList.forEach((item) => {
+            this.user.exchangesBorr.forEach((exchange, idx) => {
+              this.exchangeService
+                .getExchange(exchange.id)
+                .subscribe((exchangeComplete) => {
+                  if (idx === 0) {
+                    isInExchange = false;
+                  }
+                  if (item.id === exchangeComplete.itemBorrowed.id) {
+                    isInExchange = true;
+                  }
+                  if (
+                    idx === this.user.exchangesBorr.length - 1 &&
+                    !isInExchange
+                  ) {
+                    this.itemsToShow.push(item);
+                  }
+                });
+            });
+          });
+        } else {
+          this.user.toBorrowList.forEach((item) => {
+            this.itemsToShow.push(item);
+          });
+        }
+      }
     });
   }
 }
