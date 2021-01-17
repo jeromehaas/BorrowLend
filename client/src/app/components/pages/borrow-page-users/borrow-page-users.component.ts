c5dc476c55ee4cfafe16ec5b0601e5e6bf2584a4
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-01-17 19:24:22 +0100

    feat: a user is shown in the borrow-page-users only if the user is not already lending the game

--- client/src/app/components/pages/borrow-page-users/borrow-page-users.component.ts
+++ client/src/app/components/pages/borrow-page-users/borrow-page-users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ExchangeService } from 'src/app/services/exchange.service';
 import { UserService } from 'src/app/services/user.service';
 import { User } from '../../../models/user';
 
@@ -9,18 +10,44 @@ import { User } from '../../../models/user';
   styleUrls: ['./borrow-page-users.component.scss'],
 })
 export class BorrowPageUsersComponent implements OnInit {
-  usersLending: User[];
+  usersLending: User[] = [];
   itemId: number;
 
   constructor(
     private route: ActivatedRoute,
-    private userService: UserService
+    private userService: UserService,
+    private exchangeService: ExchangeService
   ) {}
 
   ngOnInit(): void {
     this.itemId = +this.route.snapshot.paramMap.get('itemId');
     this.userService.getUsersLending(this.itemId).subscribe((users) => {
-      this.usersLending = users;
+      // Check if the user is not already lending the game
+      let isAlreadyLending;
+      users.forEach((user) => {
+        if (user.exchangesLend.length) {
+          user.exchangesLend.forEach((exchange, idx) => {
+            this.exchangeService
+              .getExchange(exchange.id)
+              .subscribe((exchangeComplete) => {
+                if (idx === 0) {
+                  isAlreadyLending = false;
+                }
+                if (this.itemId === exchangeComplete.itemBorrowed.id) {
+                  isAlreadyLending = true;
+                }
+                if (
+                  idx === user.exchangesLend.length - 1 &&
+                  !isAlreadyLending
+                ) {
+                  this.usersLending.push(user);
+                }
+              });
+          });
+        } else {
+          this.usersLending.push(user);
+        }
+      });
     });
   }
 }
