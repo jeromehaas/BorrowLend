5508cda007ef0c4b48206c7d9e98a374ee9e0924
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-01-19 15:41:10 +0100

    fix: removed auto log in when opening the application

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { setItems } from './actions/items.actions';
-import { setUser } from './actions/users.actions';
 import { AppState } from './app.state';
 import { ItemsService } from './services/items.service';
-import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -15,15 +13,10 @@ export class AppComponent implements OnInit {
   title = 'client';
   constructor(
     private store: Store<AppState>,
-    private itemsService: ItemsService,
-    private userService: UserService
+    private itemsService: ItemsService
   ) {}
 
   ngOnInit(): void {
-    // Temporary, for developing purposes
-    this.userService.getUser('Alba').subscribe((user) => {
-      this.store.dispatch(setUser({ user }));
-    });
     this.itemsService.getItems().subscribe((items) => {
       this.store.dispatch(setItems({ items }));
     });
