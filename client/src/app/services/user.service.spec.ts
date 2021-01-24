8df70ca5bba25467ef49eed098b3aeb893745c37
Jerome Haas <j.haas@cyberplanet.ch>
2021-01-24 22:30:01 +0100

    Implement test for user.service

--- client/src/app/services/user.service.spec.ts
+++ client/src/app/services/user.service.spec.ts
@@ -1,16 +1,58 @@
-import { TestBed } from '@angular/core/testing';
-
 import { UserService } from './user.service';
+import { fakeAsync, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { USER } from '../testMocks/user';
+import { USERS } from '../testMocks/users';
+import { apiUrl } from '../services/apiUrl';
 
-describe('UserService', () => {
-  let service: UserService;
+fdescribe('CoursesService', () => {
+	let httpTestingController: HttpTestingController;
+	let service: UserService;
+	let mockUser: any;
+	let mockUsers: any;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [HttpClientTestingModule],
+			providers: [UserService]
+		});
+		httpTestingController = TestBed.inject(HttpTestingController);
+		service = TestBed.inject(UserService);
+		mockUser = USER;
+		mockUsers = USERS;
+	});
 
-  it('should be created', () => {
+	afterEach(() => {
+		httpTestingController = null
+		service = null;
+		mockUser = null;
+		mockUsers = null;
+	});
+
+	it('The user-service must be created', () => {
     expect(service).toBeTruthy();
-  });
+	});
+
+	it('The first user in the DB should be "Alba"', () => {
+		service.getAllUsers().subscribe((users) => {
+			expect(users[0][0].username).toEqual('Alba');
+			expect(users[0]).toEqual(mockUser);
+		});
+		const request = httpTestingController.expectOne(`${apiUrl}/users`);
+		request.flush([mockUser]);
+		httpTestingController.verify();
+	});
+
+	it('Should get "Albe" by ID', () => {
+		service.getUserById(2).subscribe((user) => {
+			console.log(user[0][0]);
+			expect(user[0][0].username).toEqual('Alba');
+		});
+		const request = httpTestingController.expectOne(`${apiUrl}/users/2`);
+		request.flush([mockUser]);
+		httpTestingController.verify();
+	});
+
+
 });
+
