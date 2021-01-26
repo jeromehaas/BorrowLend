0658fda231f5ead84f844ce6c6644414fd338d05
Alba Garcia <garmobal@Albas-Air.lan>
2021-01-26 16:27:16 +0100

    refactored tests on borrow card

--- client/src/app/components/elements/exchange-card-borrow/exchange-card-borrow.component.spec.ts
+++ client/src/app/components/elements/exchange-card-borrow/exchange-card-borrow.component.spec.ts
@@ -5,9 +5,10 @@ import { Store } from '@ngrx/store';
 import { ExchangeService } from 'src/app/services/exchange.service';
 import { UserService } from 'src/app/services/user.service';
 import { EXCHANGE_COMPLETE } from 'src/app/testMocks/exchangeService';
-import { setUser } from 'src/app/actions/users.actions';
+import { of } from 'rxjs';
 
 import { ExchangeCardBorrowComponent } from './exchange-card-borrow.component';
+import { USER } from 'src/app/testMocks/user';
 
 describe('ExchangeCardBorrowComponent', () => {
   let component: ExchangeCardBorrowComponent;
@@ -15,23 +16,16 @@ describe('ExchangeCardBorrowComponent', () => {
   let mockExchangeService;
   let mockUserService;
   let mockStore;
-  let mockSetUser;
 
   beforeEach(async () => {
 
-    mockExchangeService = jasmine.createSpyObj([
-      'deleteExchange', 'endExchange'
-    ]);
-
-    mockExchangeService.deleteExchange.and.returnValue([]);
-
+    mockExchangeService = jasmine.createSpyObj(['deleteExchange', 'endExchange']);
     mockUserService = jasmine.createSpyObj(['getUserById']);
     mockStore = jasmine.createSpyObj(['dispatch']);
 
-    mockSetUser = jasmine.createSpyObj(['setUser']);
-
-    mockSetUser.setUser.and.returnValue(2);
-
+    mockExchangeService.deleteExchange.and.returnValue(of(null));
+    mockUserService.getUserById.and.returnValue(of(USER));
+    mockStore.dispatch.and.returnValue([])
 
     await TestBed.configureTestingModule({
       declarations: [ ExchangeCardBorrowComponent ],
@@ -63,15 +57,14 @@ describe('ExchangeCardBorrowComponent', () => {
     expect(mockExchangeService.deleteExchange).toHaveBeenCalledWith(component.exchange.id);
   }));
 
-  // fit('should end an exchange when end button is clicked', fakeAsync(() => {
-  //   component.exchange.accepted = true;
-  //   component.exchange.isActiveLend = null;
-  //   component.exchange.isActiveBorr = null;
-  //   const button = fixture.debugElement.query(By.css('.endbuttontest')).nativeElement;
-  //   button.click();
-  //   tick();
-  //   expect(mockExchangeService.endExchange).toHaveBeenCalled();
+  fit('should dispatch a setUser action to update the state', fakeAsync(() => {
+    const button = fixture.debugElement.query(By.css('.deletebutton')).nativeElement;
+    button.click();
+    tick();
+    expect(mockExchangeService.deleteExchange).toHaveBeenCalledWith(component.exchange.id);
+    expect(mockUserService.getUserById).toHaveBeenCalledWith(component.exchange.userBorrowing.id);
+    expect(mockStore.dispatch).toHaveBeenCalled();
 
-  // }));
+  }));
 
 });
