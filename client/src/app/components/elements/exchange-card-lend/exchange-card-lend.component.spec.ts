2c6753c87197a6cf6b15652ad08d56a05eb68b11
Alba Garcia <garmobal@Albas-Air.lan>
2021-01-26 11:49:54 +0100

    lend card tests

--- client/src/app/components/elements/exchange-card-lend/exchange-card-lend.component.spec.ts
+++ client/src/app/components/elements/exchange-card-lend/exchange-card-lend.component.spec.ts
@@ -1,14 +1,37 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { ExchangeService } from 'src/app/services/exchange.service';
 import { ExchangeCardLendComponent } from './exchange-card-lend.component';
+import { Store } from '@ngrx/store';
+import { UserService } from 'src/app/services/user.service';
+import { EXCHANGE_COMPLETE } from 'src/app/testMocks/exchangeService';
+
 
 describe('ExchangeCardLendComponent', () => {
   let component: ExchangeCardLendComponent;
   let fixture: ComponentFixture<ExchangeCardLendComponent>;
+  let mockExchangeService;
+  let mockUserService;
+  let mockStore;
 
   beforeEach(async () => {
+
+    mockExchangeService = jasmine.createSpyObj([
+      'deleteExchange', 'endExchange', 'acceptExchange', 'rejectExchange'
+    ]);
+
+    mockUserService = jasmine.createSpyObj(['getUserById']);
+    mockUserService.getUserById.and.returnValue([]);
+    
     await TestBed.configureTestingModule({
-      declarations: [ ExchangeCardLendComponent ]
+      declarations: [ ExchangeCardLendComponent ],
+      imports: [ HttpClientTestingModule ],
+      providers: [
+        { provide: ExchangeService, useValue: mockExchangeService },
+        { provide: UserService, useValue: mockUserService },
+        { provide: Store, useValue: mockStore }
+      ]
     })
     .compileComponents();
   });
@@ -16,10 +39,29 @@ describe('ExchangeCardLendComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ExchangeCardLendComponent);
     component = fixture.componentInstance;
+    component.exchange = EXCHANGE_COMPLETE;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  fit('should create card lend component', () => {
     expect(component).toBeTruthy();
   });
+
+  fit('should accept an exchange when accept button is clicked', fakeAsync(() => {
+    const button = fixture.debugElement.query(By.css('.buttons__accept')).nativeElement;
+    button.click();
+    tick();
+    expect(mockExchangeService.acceptExchange).toHaveBeenCalledWith(component.exchange.id);
+  }));
+
+  fit('should reject an exchange when reject button is clicked', fakeAsync(() => {
+    const button = fixture.debugElement.query(By.css('.buttons__reject')).nativeElement;
+    button.click();
+    tick();
+    expect(mockExchangeService.rejectExchange).toHaveBeenCalledWith(component.exchange.id);
+    // tick();
+    // expect(mockUserService.getUserById).toHaveBeenCalled()
+  }));
+
+  
 });
